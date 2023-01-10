1d0ce3616d3d6930bb58f5eb8595f249afaefc17
Ngarison <jules.ngari@gmail.com>
2023-01-10 09:41:30 +0100

    Mise à jour de mon exercice

--- ex00bis/script.js
+++ ex00bis/script.js
@@ -1,10 +1,44 @@
 $(document).ready(function(){
+     let borderRadius=0;
+     let colors=["green","blue","red"];
+     let i=0;
     $("#ballon").click(function(){
-
         //console.log($("#ballon").width())
         $("#ballon").css({"width":$("#ballon").width()+10+"px"});
         $("#ballon").css({"height":$("#ballon").height()+10+"px"});
-        $("#ballon").css({"border-radius":$("#ballon").border-radius()+10+"px"});
+        $("#ballon").css("border-radius",borderRadius+=10);
+        $("#ballon").css("background-color",colors[i]);
+
+        if(i>=2){
+            i=0;
+        }else{
+            i++;
+        }
+
+        if($("#ballon").width()>=420){
+            $("#ballon").css({"width":200+"px"});
+            $("#ballon").css({"height":200+"px"});
+            $("#ballon").css("border-radius",100);
+        }
+
+        let K=i;
+
+        $("#ballon").hover(function(){
+            alert("yes!")
+          },
+          function(){
+            if(k<=0){
+                k=2;
+            }else{
+                k--;
+            }
+            // $("#ballon").css("background-color",colors[k]);
+            alert("test réussi!");
+
+
+          });
+
+
     });
 
   });
